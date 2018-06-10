ddb55c34f79addef7502af4cba0bec6124f49ab1
ykum <ykum@ebay.com>
2018-06-10 22:46:20 +0900

    init

---
+++ cashstore/src/main/java/com/ebay/boot/cashstore/CashstoreApplication.java
@@ -0,0 +1,12 @@
+package com.ebay.boot.cashstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashstoreApplication.class, args);
+	}
+}
